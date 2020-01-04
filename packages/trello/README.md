1620315c0a0e3b36ec9d0007f1aab49c4df31b34
cboschmans-code <c.boschmans@studie.openuniversiteit.be>
2020-01-04 22:15:37 +0000

    added trello package

---
+++ packages/trello/README.md
@@ -0,0 +1,66 @@
+<!--
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+-->
+
+# Using the Trello package
+
+The `/whisk.system/trello` package offers a convenient way to use the [Trello REST API](https://developers.trello.com/reference#tokenstoken-1).
+
+The package includes the following feed:
+
+| Entity | Type | Parameters | Description |
+| --- | --- | --- | --- |
+| `/whisk.system/trello` | package | username, repository, accessToken | Interact with the Trello API |
+| `/whisk.system/trello/webhook` | feed | events, username, repository, accessToken | Fire trigger events on Trello activity |
+
+Creating a package binding with the `APIKey`, `idModel`, `accessToken` and `description` values is suggested.  With binding, you don't need to specify the values each time that you use the feed in the package.
+
+## Firing a trigger event with GitHub activity
+
+The `/whisk.system/trello/webhook` feed configures a service to fire a trigger when there is activity in a specified GitHub repository. The parameters are as follows:
+
+- `APIKey`: The  developer API key see [Trello API key security](https://developers.trello.com/docs/api-key-security).
+- `idModel`: id of card, list or board of interest [How to get the id of your model of interest](https://stackoverflow.com/questions/26552278/trello-api-getting-boards-lists-cards-information)
+- `accessToken`: Your Trello personal server token. 
+- `description`: description of your webhook
+
+The following is an example of creating a trigger that will be fired each time that there is a new commit to a GitHub repository.
+
+
+
+1. Create a package binding that is configured for your Trello object model (for example card, list, board model). See [Trello authorization](https://developers.trello.com/page/authorization) how to get the server token an  API key
+
+  ```
+  wsk package bind /whisk.system/trello myTrello \
+    --param APIKey 1245788745 \
+    --param idModel 12214551 \
+    --param accessToken 4887454512 \
+    --param description mydescription 
+  ```
+
+3. Create a trigger by using your `myTrello/webhook` feed.
+
+  ```
+  wsk trigger create myTrelloTrigger --feed myTrello/webhook 
+  ```
+
+  A change  to the prefered Trello model  causes the trigger to be fired by the webhook. If there is a rule that matches the trigger, then the associated action will be invoked.
+
+  The action receives the Trello webhook payload as an input parameter.
+
+  For more information about the payload content, see the [Trello webhooks] (https://developers.trello.com/page/webhooks).
---
+++ packages/trello/webhook.js
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const needle = require('needle');
+
+/**
+ *  Feed to create a webhook on Trello
+ *
+ *  @param {object} params - information about the trigger
+ *  @param {string} APIKey  - trello API key
+ *  @param {string} accessToken - trello API token
+ *  @param {string} idModel -id of card, list, board
+ *  @param {string} description - description of hook
+ *  @return {object} whisk async
+ */
+function main(params) {
+  var description = params.description;
+  var accessToken = params.accessToken;
+  var APIkey =params.APIkey;
+  var idModel=params.idModel;
+
+
+  var lifecycleEvent = params.lifecycleEvent;
+  var triggerName = params.triggerName.split("/");
+
+  // URL of the whisk system. The calls of github will go here.
+  var urlHost = require('url').parse(process.env.__OW_API_HOST);
+  var whiskCallbackUrl = urlHost.protocol + '//' + process.env.__OW_API_KEY + "@" + urlHost.host + '/api/v1/namespaces/' + encodeURIComponent(triggerName[1]) + '/triggers/' + encodeURIComponent(triggerName[2]);
+
+  // The URL to create the webhook on Github
+  var registrationEndpoint = 'https://api.trello.com/1/tokens/' + accessToken + '/webhooks;
+  console.log("Using endpoint: " + registrationEndpoint);
+
+  if (lifecycleEvent === 'CREATE') {
+   // var events = params.events.split(',');
+
+   /* var body = {
+      name: 'web',
+      active: true,
+      events: events,
+      config: {
+        url: whiskCallbackUrl,
+        content_type: 'json'
+      }
+    };
+*/
+    var promise = new Promise(function (resolve, reject) {
+      needle.post(registrationEndpoint+'?key='+APIkey, body, { 'json': true, description: description, callbackURL: whiskCallbackUrl, idModel: idModel }, function (error, response, body) {
+        if (error) {
+          reject({
+            response: response,
+            error: error,
+            body: body
+          });
+        } else {
+          console.log("Status code: " + response.statusCode);
+
+          if (response.statusCode >= 400) {
+            console.log("Response from Trello: " + body);
+            reject({
+              statusCode: response.statusCode,
+              response: body
+            });
+          } else {
+            resolve({ response: body });
+          }
+        }
+      });
+    });
+
+    return promise;
+  } else if (lifecycleEvent === 'DELETE') {
+    //list all the existing webhooks first.
+    var deletePromise = new Promise(function (resolve, reject) {
+      needle.get(registrationEndpoint'?key='+APIkey, function (error, response, body) {
+
+        var foundWebhookToDelete = false;
+
+        if (error) {
+          reject({
+            response: response,
+            error: error,
+            body: body
+          });
+        } else {
+          for (var i = 0; i < body.length; i++) {
+            if (decodeURI(body[i].callbackURL) === whiskCallbackUrl) {
+              foundWebhookToDelete = true;
+
+              console.log('DELETE Webhook : ' + body[i].description);
+
+              needle.delete(registrationEndpoint +'/' +body[i].id +'?key='+APIkey, null, function (error, response, body) {
+                if (error) {
+                  reject({
+                    response: response,
+                    error: error,
+                    body: body
+                  });
+                } else {
+                  console.log("Status code: " + response.statusCode);
+                  if (response.statusCode >= 400) {
+                    console.log("Response from Github: " + body);
+
+                    // a 404 is common and confusing enough to warrant an extra message
+                    if (response.statusCode === 404) {
+                      console.log('Please ensure your accessToken is authorized to delete webhooks.');
+                    }
+
+                    reject({
+                      statusCode: response.statusCode,
+                      response: body
+                    });
+                  } else {
+                    resolve();
+                  }
+                }
+              });
+            }
+          }
+
+          if (!foundWebhookToDelete) {
+            reject('Found no existing webhooks for trigger URL ' + whiskCallbackUrl);
+          }
+        }
+      });
+    });
+
+    return deletePromise;
+  }
+}
